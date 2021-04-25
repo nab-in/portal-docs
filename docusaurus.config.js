29c851022596675237b4bddcb343a2dd0d6869d7
Bennett <bennybenester@gmail.com>
2021-04-25 20:25:09 +0300

    remove unused lines of code

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ module.exports = {
         },
         // { to: "/blog", label: "Blog", position: "left" },
         {
-          href: "https://github.com/facebook/docusaurus",
+          href: "https://github.com/nab-in/portal-docs/",
           label: "GitHub",
           position: "right",
         },
@@ -33,47 +33,6 @@ module.exports = {
     },
     footer: {
       style: "dark",
-      // links: [
-      //   {
-      //     title: "Docs",
-      //     items: [
-      //       {
-      //         label: "Tutorial",
-      //         to: "/docs/intro",
-      //       },
-      //     ],
-      //   },
-      //   // {
-      //   //   title: "Community",
-      //   //   items: [
-      //   //     {
-      //   //       label: "Stack Overflow",
-      //   //       href: "https://stackoverflow.com/questions/tagged/docusaurus",
-      //   //     },
-      //   //     {
-      //   //       label: "Discord",
-      //   //       href: "https://discordapp.com/invite/docusaurus",
-      //   //     },
-      //   //     {
-      //   //       label: "Twitter",
-      //   //       href: "https://twitter.com/docusaurus",
-      //   //     },
-      //   //   ],
-      //   // },
-      //   // {
-      //   //   title: "More",
-      //   //   items: [
-      //   //     {
-      //   //       label: "Blog",
-      //   //       to: "/blog",
-      //   //     },
-      //   //     {
-      //   //       label: "GitHub",
-      //   //       href: "https://github.com/facebook/docusaurus",
-      //   //     },
-      //   //   ],
-      //   // },
-      // ],
       copyright: `Copyright © ${new Date().getFullYear()} (We Code Together) with ❣️.`,
     },
   },
@@ -83,15 +42,7 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/",
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+          editUrl: "https://github.com/nab-in/portal-docs/edit/main/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
